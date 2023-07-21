52ef23f0057bb76703b15ceb17d276d263677124
sharan7898 <gssharan7898@gmail.com>
2023-07-21 12:05:09 +0530

    changed to check ci/cd

--- src/main/java/com/ActionDemo/cicd/ActionDemoApplication.java
+++ src/main/java/com/ActionDemo/cicd/ActionDemoApplication.java
@@ -2,9 +2,21 @@ package com.ActionDemo.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ActionDemoApplication {
+	
+	@GetMapping("/welcome")
+	public String Welcome() {
+		
+		return "Welcome to CI/CD Demo "
+				+ "CI/CD is working";
+		
+		
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActionDemoApplication.class, args);
